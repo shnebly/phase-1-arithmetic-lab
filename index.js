6e001279c121324a42645c5b9c468072aee206da
Shelby Morris <shelbynicole.morris@gmail.com>
2023-02-01 21:33:35 -0700

    complete all math tests

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 8;
+let num4 = 68;
+let num5 = 20;
+let multiply = num1 * num2; 
+let random = Math.ceil(Math.random());
+let mod = num4 % num3;
+let max = Math.max(num2, num3, num5);
